bf3117dafc04f357dc3938cc9ab92cb0a715d5c5
AuJung <nuttapong6280@gmail.com>
2024-11-14 10:12:34 +0700

    feat: download file from datalake

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from src.azure_datalake import download_folder
 
 
 if __name__ == '__main__':
+    download_folder('thanapat-blob-poc', 'Mock_Organization_xxdgg2i0rwi2t40dollje22p/records/1731389047521_ivwupr2qc5tn54j94yii0laf','./downloaded')
