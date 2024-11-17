88923a4adf0b52fa5a66369ef757e5f21d866454
AuJung <nuttapong6280@gmail.com>
2024-11-17 10:13:59 +0700

    feat: location analysis

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # from src.tloc_decoder import read_location_binary
-# from src.azure_datalake import download_folder
+from src.azure_datalake import download_folder
 # from src.information_read import read_session_information
+from src.process_video import process_video
 
 if __name__ == '__main__':
-    print('Hello World')
-    # download_folder('thanapat-blob-poc', 'Mock_Organization_xxdgg2i0rwi2t40dollje22p/records/1731412651867_ivwupr2qc5tn54j94yii0laf','./downloaded')
+    print("Hello World")
+    # download_folder('thanapat-blob-poc', 'Mock_Organization_xxdgg2i0rwi2t40dollje22p/records/1731413558273_ivwupr2qc5tn54j94yii0laf','./downloaded')
     # session_information = read_session_information()
+    # process_video('./downloaded/1731412651889.mp4')
--- src/process_video.py
+++ src/process_video.py
@@ -1,21 +1,42 @@
 
 from threading import Condition
 from queue import Queue
+from ultralytics import YOLO
 
+import cv2
 
-detection_queue = Queue()
-detection_thread_condition = Condition()
+model = YOLO('../model/best.pt')
+
+# detection_queue = Queue()
+# detection_thread_condition = Condition()
 
 
 def process_video(video_path: str):
     try:
-        with open(video_path, 'rb') as video_file:
-            # TODO: Process video file
-            data = video_file.read()
-            return data
-    except FileNotFoundError:
-        print(f'File not found: {video_path}')
-        return None
+        frame_count = 0
+        video_capture = cv2.VideoCapture(video_path)
+
+        while video_capture.isOpened():
+            ret, frame = video_capture.read()
+
+            if not ret:
+                break
+
+            frame_count += 1
+            if frame_count % 10 == 0:
+                results = model(frame)
+
+                for result in results:
+                    for box in result.boxes:
+                        x1, y1, x2, y2 = map(int, box.xyxy[0])
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+                # with detection_thread_condition:
+                #     while detection_queue.full():
+                #         detection_thread_condition.wait()
+                #     detection_queue.put(frame)
+                #     detection_thread_condition.notify_all()
+
     except Exception as e:
         print(f'Error processing video: {e}')
         return None
